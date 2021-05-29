73158e391a4b92dbd10fa17940530dd54b52e0a1
Yaseen-Haffejee <65509517+Yaseen-Haffejee@users.noreply.github.com>
2021-05-29 20:49:20 +0200

    new setup
    restructured and added homepage with sound

---
+++ CGV Project/js/Controls.js
@@ -0,0 +1,226 @@
+import * as THREE from 'https://cdn.jsdelivr.net/npm/three@0.118/build/three.module.js';
+import {FBXLoader} from 'https://cdn.jsdelivr.net/npm/three@0.118.1/examples/jsm/loaders/FBXLoader.js';
+import {CharacterController} from './States.js';
+class BasicCharacterControllerProxy {
+    constructor(animations) {
+      this.allAnimations = animations;
+    }
+  
+    get animations() {
+      return this.allAnimations;
+    }
+  };
+  
+  
+  class BasicCharacterController {
+    constructor(paramaters) {
+      this.init(paramaters);
+    }
+  
+    init(paramaters) {
+      this.params = paramaters;
+      this.Decrease = new THREE.Vector3(-0.0005, -0.0001, -5.0);
+      this.acceleration = new THREE.Vector3(1, 0.25, 50.0);
+      this.speed = new THREE.Vector3(0, 0, 0);
+      this.myPosition = new THREE.Vector3();
+  
+      this.allAnimations = {};
+      this.UserInput = new BasicCharacterControllerInput();
+      this.myState = new CharacterController(
+          new BasicCharacterControllerProxy(this.allAnimations));
+  
+      this.LoadModels();
+    }
+  
+    LoadModels() {
+      const loader = new FBXLoader();
+      loader.setPath('./models/Character/');
+      loader.load('aj.fbx', (fbx) => {
+        fbx.scale.setScalar(0.1);
+        fbx.traverse(c => {
+          c.castShadow = true;
+        });
+  
+        this.target = fbx;
+        this.params.scene.add(this.target);
+  
+        this.Mixer = new THREE.AnimationMixer(this.target);
+  
+        this.loadingManager = new THREE.LoadingManager();
+        this.loadingManager.onLoad = () => {
+          this.myState.setState('idle');
+        };
+  
+        const OnLoad = (animName, anim) => {
+          const clip = anim.animations[0];
+          const action = this.Mixer.clipAction(clip);
+    
+          this.allAnimations[animName] = {
+            clip: clip,
+            action: action,
+          };
+        };
+  
+        const loader = new FBXLoader(this.loadingManager);
+        loader.setPath('./models/Character/');
+        loader.load('walk.fbx', (a) => { OnLoad('walk', a); });
+        loader.load('run.fbx', (a) => { OnLoad('run', a); });
+        loader.load('idle.fbx', (a) => { OnLoad('idle', a); });
+        loader.load('Jumping Up.fbx', (a) => { OnLoad('Jumping Up', a); });
+      });
+    }
+  
+    get Position() {
+      return this.myPosition;
+    }
+  
+    get Rotation() {
+      if (!this.target) {
+        return new THREE.Quaternion();
+      }
+      return this.target.quaternion;
+    }
+  
+    Update(timeInSeconds) {
+      if (!this.myState.CurrentState) {
+        return;
+      }
+  
+      this.myState.Update(timeInSeconds, this.UserInput);
+  
+      const velocity = this.speed;
+      const frameDecceleration = new THREE.Vector3(
+          velocity.x * this.Decrease.x,
+          velocity.y * this.Decrease.y,
+          velocity.z * this.Decrease.z
+      );
+      frameDecceleration.multiplyScalar(timeInSeconds);
+      frameDecceleration.z = Math.sign(frameDecceleration.z) * Math.min(
+          Math.abs(frameDecceleration.z), Math.abs(velocity.z));
+  
+      velocity.add(frameDecceleration);
+  
+      const controlObject = this.target;
+      const quaternion = new THREE.Quaternion();
+      const a = new THREE.Vector3();
+      const r = controlObject.quaternion.clone();
+  
+      const acc = this.acceleration.clone();
+      if (this.UserInput.keys.shift) {
+        acc.multiplyScalar(4.0);
+      }
+  
+      if (this.myState.CurrentState.Name == 'Jumping Up') {
+        acc.multiplyScalar(0.0);
+      }
+  
+      if (this.UserInput.keys.forward) {
+        velocity.z += acc.z * timeInSeconds;
+      }
+      if (this.UserInput.keys.backward) {
+        velocity.z -= acc.z * timeInSeconds;
+      }
+      if (this.UserInput.keys.left) {
+        a.set(0, 1, 0);
+        quaternion.setFromAxisAngle(a, 4.0 * Math.PI * timeInSeconds * this.acceleration.y);
+        r.multiply(quaternion);
+      }
+      if (this.UserInput.keys.right) {
+        a.set(0, 1, 0);
+        quaternion.setFromAxisAngle(a, 4.0 * -Math.PI * timeInSeconds * this.acceleration.y);
+        r.multiply(quaternion);
+      }
+  
+      controlObject.quaternion.copy(r);
+  
+      const oldPosition = new THREE.Vector3();
+      oldPosition.copy(controlObject.position);
+  
+      const forward = new THREE.Vector3(0, 0, 1);
+      forward.applyQuaternion(controlObject.quaternion);
+      forward.normalize();
+  
+      const sideways = new THREE.Vector3(1, 0, 0);
+      sideways.applyQuaternion(controlObject.quaternion);
+      sideways.normalize();
+  
+      sideways.multiplyScalar(velocity.x * timeInSeconds);
+      forward.multiplyScalar(velocity.z * timeInSeconds);
+  
+      controlObject.position.add(forward);
+      controlObject.position.add(sideways);
+  
+      this.myPosition.copy(controlObject.position);
+  
+      if (this.Mixer) {
+        this.Mixer.update(timeInSeconds);
+      }
+    }
+  };
+  class BasicCharacterControllerInput {
+    constructor() {
+      this.init();    
+    }
+  
+    init() {
+      this.keys = {
+        forward: false,
+        backward: false,
+        left: false,
+        right: false,
+        space: false,
+        shift: false,
+      };
+      document.addEventListener('keydown', (e) => this.OnKeyDown(e), false);
+      document.addEventListener('keyup', (e) => this.OnKeyUp(e), false);
+    }
+  
+    OnKeyDown(event) {
+      switch (event.keyCode) {
+        case 87: // w
+          this.keys.forward = true;
+          break;
+        case 65: // a
+          this.keys.left = true;
+          break;
+        case 83: // s
+          this.keys.backward = true;
+          break;
+        case 68: // d
+          this.keys.right = true;
+          break;
+        case 32: // SPACE
+          this.keys.space = true;
+          break;
+        case 16: // SHIFT
+          this.keys.shift = true;
+          break;
+      }
+    }
+  
+    OnKeyUp(event) {
+      switch(event.keyCode) {
+        case 87: // w
+          this.keys.forward = false;
+          break;
+        case 65: // a
+          this.keys.left = false;
+          break;
+        case 83: // s
+          this.keys.backward = false;
+          break;
+        case 68: // d
+          this.keys.right = false;
+          break;
+        case 32: // SPACE
+          this.keys.space = false;
+          break;
+        case 16: // SHIFT
+          this.keys.shift = false;
+          break;
+      }
+    }
+  };
+  
+
+  export{BasicCharacterController}
